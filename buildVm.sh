036060825b04cfb30d5fea3079ca5437844133d0
akram09 <a.boutouchent@esi-sba.dz>
2021-05-17 03:18:10 +0100

    Added virtual box creation

---
+++ buildVm.sh
@@ -0,0 +1,20 @@
+MACHINENAME=$1
+
+
+#Create VM
+VBoxManage createvm --name $MACHINENAME --ostype "Debian_64" --register --basefolder `pwd`
+#Set memory and network
+VBoxManage modifyvm $MACHINENAME --ioapic on
+VBoxManage modifyvm $MACHINENAME --memory 1024 --vram 128
+VBoxManage modifyvm $MACHINENAME --nic1 nat
+#Create Disk and connect Debian Iso
+VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi --size 15000 --format VDI
+VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
+VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
+VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/$MACHINENAME.iso
+VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
+#Enable RDP
+VBoxManage modifyvm $MACHINENAME --vrde on
+VBoxManage modifyvm $MACHINENAME --vrdemulticon on --vrdeport 10001
