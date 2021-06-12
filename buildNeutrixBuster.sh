d31319e17b21963266b97382725e1d97cc7a2ee0
AbdouTlili <ha_tlili@esi.dz>
2021-06-12 16:16:22 +0100

    droping extra packages and enabling calamares installer

--- buildNeutrixBuster.sh
+++ buildNeutrixBuster.sh
@@ -49,6 +49,8 @@ lb config --binary-images iso-hybrid \
 echo "task-$DESKTOP_ENV-desktop" > $WKDIR/neutrixOS/config/package-lists/desktop.list.chroot
 #
 #
+# the apps to install : 
+
 echo haveged less \
         orage gdebi \
         galculator grsync \
@@ -121,7 +123,7 @@ echo haveged less \
         guvcview > $WKDIR/neutrixOS/config/package-lists/packages.list.chroot
 #
 
-# the apps to install : 
+# boot apps to install : 
 echo grub-common \
         grub2-common \
         grub-pc-bin \
